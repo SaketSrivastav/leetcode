5d7c7b61c5ca7efb41dbb10f210e36b48d76e55e
SaketSrivastav <saketsrivastav79@gmail.com>
2018-09-03 09:33:14 -0700

    comments on sum_of_sq soln about greedy and DP.

--- sum_of_sq.cc
+++ sum_of_sq.cc
@@ -34,6 +34,7 @@ int sum_of_sq(int n) {
 }
 
 
+// Correct solution: uses DP and builds the table bottom up
 int sum_of_sq_dp(int n) {
   // Create a dynamic programming table
   // to store sq
@@ -53,12 +54,12 @@ int sum_of_sq_dp(int n) {
   {
     // max value is i as i can always be represented
     // as 1*1 + 1*1 + ...
-    // cout << endl;
-    // for(int i=0; i<dp.size(); i++)
-    //   printf("%d ", dp[i]);
-    // cout << endl;
+    cout << endl;
+    for(int i=0; i<dp.size(); i++)
+      printf("%d ", dp[i]);
+    cout << endl;
     dp[i] = i;
-    // printf("\ndp[%d] = %d\n", i, dp[i]);
+    printf("\ndp[%d] = %d\n", i, dp[i]);
 
     // Go through all smaller numbers to
     // to recursively find minimum
@@ -66,13 +67,13 @@ int sum_of_sq_dp(int n) {
       int temp = x*x;
       if (temp > i)
       {
-        // printf("tmp %d > i %d\n", temp, i);
+        printf("tmp %d > i %d\n", temp, i);
         break;
       }
       else{
-        // printf("dp[%d] = min(dp[%d], 1+dp[%d]) = %d\n",
-        //        i, dp[i], (i-temp),
-        //        min(dp[i], 1+dp[i-temp]));
+        printf("dp[%d] = min(dp[%d], 1+dp[%d]) = %d\n",
+               i, dp[i], (i-temp),
+               min(dp[i], 1+dp[i-temp]));
         dp[i] = min(dp[i], 1+dp[i-temp]);
       }
     }
@@ -84,8 +85,45 @@ int sum_of_sq_dp(int n) {
   return res;
 }
 
+// Greedy solution which computes the perfect squares top down from sqrt(n) -> 0
+// Counter example :
+//      22 => Greedy soln: 4 = (4^2 + 2^2 + 1^1 + 1^1)
+//            DP soln: 3 = 3^2 + 3^2 + 2^2
+int sum_of_sq_gr(int n) {
+  vector<int> l;
+  int d = 0;
+
+  if(n <= 0) return 0;
+
+  d = n;
+
+  while(d != 0) {
+    int sum = 0;
+    int sq_n = 0;
+    sq_n = sqrt(d);
+    cout << "sq_n " << sq_n << " d = " << d << endl;
+    l.push_back(sq_n);
+    for (int i = 0; i < l.size(); ++i) {
+      cout << "summing " << l[i] << endl;
+      sum += (l[i] * l[i]);
+    }
+
+    cout << "sum = " << sum << endl;
+    d = n - sum;
+  }
+
+      cout << "Perfect squares used: " << endl;
+    for (int i = 0; i < l.size(); ++i) {
+      cout << l[i] << " -> "<< l[i]*l[i] << " " ;
+    }
+    cout << endl;
+
+  return l.size();
+}
+
 void test_sum_of_sq(int n) {
   cout << "sum_of_sq_dp(" << n << "): "<< sum_of_sq_dp(n) << endl;
+  cout << "sum_of_sq_gr(" << n << "): "<< sum_of_sq_gr(n) << endl;
   // cout << "sum_of_sq(" << n << "): "<< sum_of_sq(n) << endl;
 }
 
