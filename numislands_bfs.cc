69b29cfa61cfc4b293dce1e922630caba3b7b201
SaketSrivastav <saketsrivastav79@gmail.com>
2018-09-03 11:18:35 -0700

    Implement DFS to calculate numislands from a 2d grid.

--- numislands_bfs.cc
+++ numislands_bfs.cc
@@ -30,7 +30,7 @@ using namespace std;
 //   Apply BFS for all the connected 1's in the grid.
 //   Number of BFS runs constitutes the number of islands present in the grid.
 
-int numislands(vector<vector<char>> & grid) {
+int numislands_bfs(vector<vector<char>> & grid) {
 
   int row;
   int col;
@@ -99,14 +99,57 @@ int numislands(vector<vector<char>> & grid) {
   return island;
 }
 
+void dfs(vector<vector<char>> & grid, int x, int y, vector<vector<bool>> & visited) {
+  // if out of bounds or at a cell with '0' or '*', simply stop and return | end the dfs
+  if(x < 0 || x >= grid.size() || y < 0 || y >= grid[0].size() ||
+     grid[x][y] != '1' || visited[x][y]) {
+    cout << "return (" << x << ", " << y << ")" << endl;
+    return;
+  }
+
+  cout << "Analysing: (" << x << ", " << y << ")"<< endl;
+  visited[x][y] = true;
+
+  // Call to all neighbors
+  dfs(grid, x + 1, y, visited);
+  dfs(grid, x - 1, y, visited);
+  dfs(grid, x, y + 1, visited);
+  dfs(grid, x, y - 1, visited);
+}
+
+int numislands_dfs(vector<vector<char>> & grid) {
+  int row;
+  int col;
+  int island = 0;
+
+  if(grid.empty()) return 0;
+  row = grid.size();
+  col = grid[0].size();
+
+  vector<vector<bool>> visited(row, vector<bool>(col, false));
+
+  for (int i=0; i < row; ++i) {
+    for(int j=0; j<col; ++j) {
+      if(grid[i][j] == '1') {
+        if(visited[i][j]) continue;
+
+        cout << "Found an island at: (" << i << ", " << j << ")" << endl;
+        island++;
+        dfs(grid, i, j, visited);
+      }
+    }
+  }
+
+  return island;
+}
+
 int main(int argc, char *argv[])
 {
-  #if 0
-  vector<vector<int>> grid = {
-    {1,1,1,1,0},
-    {1,1,0,1,0},
-    {1,1,0,0,0},
-    {0,0,0,0,0}
+  vector<vector<char>> grid = {
+    {'1','1','1','1','0'},
+    {'1','1','0','1','0'},
+    {'1','1','0','0','0'},
+    {'0','0','0','0','0'}
   };
 
   cout << "Input: " << endl;
@@ -117,10 +160,14 @@ int main(int argc, char *argv[])
     cout << endl;
   }
 
-  int num_islands = numislands(grid);
+  int num_islands = numislands_bfs(grid);
 
-  cout << "Number of islands: " << num_islands << endl;
-#endif
+  cout << "Number of islands(BFS): " << num_islands << endl;
+
+  num_islands = numislands_dfs(grid);
+
+  cout << "Number of islands(DFS): " << num_islands << endl;
+
   vector<vector<char>> grid1 = {
     {'1','1','0','0','0'},
     {'1','1','0','0','0'},
@@ -136,8 +183,14 @@ int main(int argc, char *argv[])
     cout << endl;
   }
 
-  int num_islands = numislands(grid1);
-  cout << "Number of islands: " << num_islands << endl;
+  num_islands = numislands_bfs(grid1);
+
+  cout << "Number of islands(BFS): " << num_islands << endl;
+
+  num_islands = numislands_dfs(grid1);
+
+  cout << "Number of islands(DFS): " << num_islands << endl;
+
 
   return 0;
 }
